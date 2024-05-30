07b0f6cff202e8f34860a31ba06c65beb9071a72
Anna Huang <Annah998936@gmail.com>
2024-05-29 22:59:49 -0700

    updated function codes

--- diffusion.py
+++ diffusion.py
@@ -9,6 +9,9 @@ import jax
 def calculate_mse(f, f_new):
     return mean_squared_error(f, f_new)
 
+# def calculate_mse(u_true, u_pred):
+#     return np.mean((u_true - u_pred) ** 2)
+
 def rbf_kernel_derivative(x1, x2, length_scale, order=1):
     rbf = kernels.RBF(length_scale=length_scale)
     K = rbf(x1, x2)
@@ -23,11 +26,16 @@ def P(S, s, P_coef, rbf):
     K_sS = rbf(S, s)
     return K_sS @ P_coef
 
-def smooth_and_evaluate_rbf(u_diff_noisy, x, l, Nx):
+def smooth_rbf_kernel(u_diff, u_diff_noisy, x, l, Nx):
+    # RBF kernel smoothing function
     rbf = kernels.RBF(length_scale=l)
     u_rbf = rbf(x[:, None], x[:, None])
     u_diff_smooth = u_rbf @ solve((u_rbf + 0.1 * np.eye(Nx)), u_diff_noisy)
     
+    mse_u = calculate_mse(u_diff, u_diff_smooth)
+    return mse_u, u_diff_smooth, u_rbf
+
+def compute_derivatives_and_evaluate(u_diff_smooth, u_rbf, x, l, Nx):
     f = np.cos(2 * np.pi * x)
     
     u_kx = rbf_kernel_derivative(x[:, None], x[:, None], length_scale=l, order=1).squeeze() @ solve(
@@ -36,12 +44,42 @@ def smooth_and_evaluate_rbf(u_diff_noisy, x, l, Nx):
     u_kxx = rbf_kernel_derivative(x[:, None], x[:, None], length_scale=l, order=2).squeeze() @ solve(
         (u_rbf + 0.1 * np.eye(Nx)), u_diff_smooth
     )
+    
     s = np.hstack([u_diff_smooth, u_kx, u_kxx])
+    rbf = kernels.RBF(length_scale=l)
     K = rbf(s, s)
     P_coef = solve((K + 0.01 * np.eye(Nx)), f)
     f_new = P(s, s, P_coef, rbf)
     
-    return calculate_mse(f, f_new), u_diff_smooth
+    mse = calculate_mse(f, f_new)
+    return mse, u_kx, u_kxx
+
+
+def smooth_poly_kernel(u_diff, u_diff_noisy, x, deg, coef, gam, Nx):
+    u_poly = sklearn.metrics.pairwise.polynomial_kernel(x[:, np.newaxis], x[:, np.newaxis], degree=deg, coef0=coef, gamma=gam)
+    u_diff_smooth = u_poly @ solve((u_poly + 0.1 * np.eye(Nx)), u_diff_noisy)
+    
+    mse_u = calculate_mse(u_diff, u_diff_smooth)
+    return mse_u, u_diff_smooth
+
+def compute_derivatives_and_evaluate_poly(u_diff_smooth, x, deg, coef, gam, Nx):
+    f = np.cos(2 * np.pi * x)
+    sk = sklearn.metrics.pairwise.polynomial_kernel(x[:, np.newaxis], x[:, np.newaxis], degree=deg, coef0=1, gamma=1)
+
+    u_kx = jnp.gradient(sk, axis=0) @ solve((sk + 0.1 * np.eye(Nx)), u_diff_smooth)
+    u_kxx = jnp.gradient(jnp.gradient(sk, axis=0), axis=0) @ solve((sk + 0.1 * np.eye(Nx)), u_diff_smooth)
+
+    s = np.hstack([u_diff_smooth, u_kx, u_kxx])
+
+    K = sklearn.metrics.pairwise.polynomial_kernel(s, s, degree=deg, coef0=coef, gamma=gam)
+    P_coef = solve((K + 0.01 * np.eye(Nx)), f)
+    K_sS = sklearn.metrics.pairwise.polynomial_kernel(X=s, Y=s, degree=deg, coef0=coef, gamma=gam)
+    f_new = K_sS @ P_coef
+
+    mse = calculate_mse(f, f_new)
+    return mse, u_kx, u_kxx
+
+
 
 def smooth_and_evaluate_poly(u_diff_noisy, x, deg, coef, gam, Nx):
     u_poly = sklearn.metrics.pairwise.polynomial_kernel(x[:, np.newaxis], x[:, np.newaxis], degree=deg, coef0=coef, gamma=gam)
@@ -63,6 +101,26 @@ def smooth_and_evaluate_poly(u_diff_noisy, x, deg, coef, gam, Nx):
     return calculate_mse(f, f_new), u_diff_smooth
 
 def smooth_and_evaluate_sigmoid(u_diff_noisy, x, l, Nx):
+    rbf = kernels.RBF(length_scale=l)
+    u_rbf = rbf(x[:, None], x[:, None])
+    u_diff_smooth = u_rbf @ solve((u_rbf + 0.1 * np.eye(Nx)), u_diff_noisy)
+    
+    f = np.cos(2 * np.pi * x)
+    
+    u_kx = rbf_kernel_derivative(x[:, None], x[:, None], length_scale=l, order=1).squeeze() @ solve(
+        (u_rbf + 0.1 * np.eye(Nx)), u_diff_smooth
+    )
+    u_kxx = rbf_kernel_derivative(x[:, None], x[:, None], length_scale=l, order=2).squeeze() @ solve(
+        (u_rbf + 0.1 * np.eye(Nx)), u_diff_smooth
+    )
+    s = np.hstack([u_diff_smooth, u_kx, u_kxx])
+    K = rbf(s, s)
+    P_coef = solve((K + 0.01 * np.eye(Nx)), f)
+    f_new = P(s, s, P_coef, rbf)
+    
+    return calculate_mse(f, f_new), u_diff_smooth
+
+def smooth_and_evaluate_rbf(u_diff_noisy, x, l, Nx):
     rbf = kernels.RBF(length_scale=l)
     u_rbf = rbf(x[:, None], x[:, None])
     u_diff_smooth = u_rbf @ solve((u_rbf + 0.1 * np.eye(Nx)), u_diff_noisy)
