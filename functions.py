e8d6a745958dafeabcff4d7df2d33b44065e65cc
Yash Bhangale <bhangaleys@gmail.com>
2024-05-30 23:14:49 -0700

    ENH: Removed sourcing terms, introduced ut on RHS, generated and saved data and images

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ import jax.numpy as jnp
 from jax import grad
 import matplotlib.pyplot as plt
 
-def kernel_derivatives(x1, x2, u_noisy, kernel, order=1, **kwargs):
+def kernel_spatial_derivatives(x1, x2, u_noisy, kernel, order=1, **kwargs):
     x1 = x1[:, None]
     x2 = x2[:, None]
     if kernel == 'rbf':
@@ -75,11 +75,11 @@ def kernel_derivatives(x1, x2, u_noisy, kernel, order=1, **kwargs):
         return sigmoid_derivatives
 
 
-def functional_form_PDE(s, s_test, fx, kernel: str, **kwargs):
+def functional_form_PDE(s, s_test, ut, kernel: str, **kwargs):
     if kernel == 'rbf':
         length_scale = kwargs.get('l')
         K = RBF(length_scale=length_scale)
-        P = K(s_test, s) @ solve((K(s, s) + 0.01 * np.eye(s.shape[0])), fx)
+        P = K(s_test, s) @ solve((K(s, s) + 0.01 * np.eye(s.shape[0])), ut)
         return P
     if kernel == 'poly':
         c = kwargs.get('coef0')
@@ -87,17 +87,17 @@ def functional_form_PDE(s, s_test, fx, kernel: str, **kwargs):
         g = kwargs.get('gamma')
         K = polynomial_kernel(X=s, Y=s, degree=d, gamma=g, coef0=c)
         K_test = polynomial_kernel(X=s_test, Y=s, degree=d, gamma=g, coef0=c)
-        P = K_test @ solve((K + 0.01 * np.eye(s.shape[0])), fx)
+        P = K_test @ solve((K + 0.01 * np.eye(s.shape[0])), ut)
         return P
     if kernel == 'sigmoid':
         g = kwargs.get('gamma')
         c = kwargs.get('coef0')
         K = sigmoid_kernel(X=s, Y=s, gamma=g, coef0 = c)
         K_test = sigmoid_kernel(X=s_test, Y=s, gamma=g, coef0 = c)
-        P = K_test @ solve((K + 0.01 * np.eye(s.shape[0])), fx)
+        P = K_test @ solve((K + 0.01 * np.eye(s.shape[0])), ut)
         return P
 
-def kernel_smoothing(u_noisy, x, kernel: str, **kwargs):
+def kernel_smoothing(u_noisy, x, kernel: str, reg=0.1, **kwargs):
     x = x[:, None]
     Nx = len(x)
     if kernel == 'rbf':
@@ -114,7 +114,7 @@ def kernel_smoothing(u_noisy, x, kernel: str, **kwargs):
         coef0 = kwargs.get('coef0')
         U = sigmoid_kernel(X=x, gamma=gamma, coef0 = coef0)
 
-    u_smooth = U @ solve((U + 0.1 * np.eye(Nx)), u_noisy)
+    u_smooth = U @ solve((U + reg * np.eye(Nx)), u_noisy)
     return u_smooth
 
 def plot_comparison(
@@ -129,4 +129,5 @@ def plot_comparison(
     axs[1].set(title="Noisy Data")
     axs[2].pcolormesh(x, y, u_smooth)
     axs[2].set(title="Smoothed Data")
-    return plt.show()
+    plt.show()
+    return fig, axs
